c7e5b1a5b23624467705e9c94a9e2956d1cd4e29
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-03-16 01:35:00 +0600

    filter generate success

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggle, toggleBrand } from '../app/features/Filter/filterSlice';
 import ProductCard from '../components/ProductCard';
 
 const Home = () => {
     const [products, setProducts] = useState([])
+    const dispatch = useDispatch();
+    const filter = useSelector(state => state.filter)
+    const { status, brand } = filter;
+
 
     useEffect(() => {
         fetch("https://moon-tech-server-pied.vercel.app/products")
@@ -11,22 +17,44 @@ const Home = () => {
     }, [])
     const activeClass = 'bg-indigo-500 text-white border-white';
 
+    let content;
+
+    if (products.length) {
+        content = products.map(product => <ProductCard key={product._id} product={product}></ProductCard>)
+    }
+    if (products.length && (status || brand.length)) {
+        content = products.filter(product => {
+            if (status) {
+                return product.status === true;
+            }
+            return product;
+        })
+            .filter(product => {
+                if (brand.length) {
+                    return brand.includes(product.brand)
+                }
+                return product;
+            })
+            .map(product => <ProductCard key={product._id} product={product}></ProductCard>)
+    }
+
+
     return (
         <div className='max-w-7xl mx-auto my-10'>
             <div className='mb-10 flex justify-end gap-5'>
-                <button className={`border px-3 py-2 rounded-full font-semibold ${activeClass}`}
-
+                <button className={`border px-3 py-2 rounded-full font-semibold ${filter.status && activeClass}`}
+                    onClick={() => dispatch(toggle())}
                 >In Stock</button>
-                <button className={`border px-3 py-2 rounded-full font-semibold`}
-
+                <button className={`border px-3 py-2 rounded-full font-semibold ${filter.brand.includes("amd") && activeClass}`}
+                    onClick={() => dispatch(toggleBrand("amd"))}
                 >AMD</button>
-                <button className={`border px-3 py-2 rounded-full font-semibold`}
-
+                <button className={`border px-3 py-2 rounded-full font-semibold ${filter.brand.includes("intel") && activeClass}`}
+                    onClick={() => dispatch(toggleBrand("intel"))}
                 >Intel</button>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
                 {
-                    products.map(product => <ProductCard key={product._id} product={product}></ProductCard>)
+                    content
                 }
             </div>
 
--- src/app/features/Filter/filterSlice.js
+++ src/app/features/Filter/filterSlice.js
@@ -9,7 +9,21 @@ const initialState = {
 const filterSlice = createSlice({
     name: "filter",
     initialState,
-    reducers: {}
+    reducers: {
+        toggle: (state, action) => {
+            state.status = !state.status;
+        },
+        toggleBrand: (state, action) => {
+            const selectedBrand = state.brand.find(brand => brand === action.payload)
+            if (!selectedBrand) {
+                state.brand.push(action.payload)
+            } else {
+                state.brand = state.brand.filter(brand => brand !== action.payload)
+            }
+
+        }
+    }
 })
+export const { toggle, toggleBrand } = filterSlice.actions;
 
 export default filterSlice.reducer;
